6989564d3e704817130bc4d1915459cda6cf73a7
Abel Noble <abel.noble3@gmailc.om>
2021-07-15 10:04:39 -0400

    included derived class of HadronGasPipiBremsstrahlung pt. 2

---
+++ src/HadronGasPipiBremsstrahlung.cpp
@@ -0,0 +1,18 @@
+
+#include "HadronGasPipiBremsstrahlung.h"
+
+HadronGasPipiBremsstrahlung::HadronGasPipiBremsstrahlung(std::shared_ptr<ParameterReader> paraRdr_in) : ThermalPhoton{paraRdr_in} {}
+
+void HadronGasPipiBremsstrahlung::analyticRates(double T, double muB, std::vector<double> &Eq, std::vector<double> &eqrate_ptr) {
+    double T2 = T*T;
+    double T3 = T*T*T;
+    for (int i = 0; i < Eq.size(); i++) {
+        double Eq_local = Eq[i];
+        double abT = -16.28 + 62.45*T - 93.4*T2 - 7.5*T3;
+        double bbT = -35.54 + 414.8*T - 2054*T2 + 3718.8*T3;
+        double ybT = 0.7364 - 10.72*T + 56.32*T2 - 103.5*T3;
+        double dbT = -2.51 + 58.152*T - 318.24*T2 + 610.7*T3;
+        double logB = abT + bbT*Eq_local + ybT*Eq_local*Eq_local + dbT*(1/(Eq_local + 0.2));
+        eqrate_ptr[i] = logB;
+    }
+}
---
+++ src/HadronGasPipiBremsstrahlung.h
@@ -0,0 +1,18 @@
+#ifndef SRC_HADRONGASPIPIBREMSSTRAHLUNG
+#define SRC_HADRONGASPIPIBREMSSTRAHLUNG
+
+#include <vector>
+#include <memory>
+
+#include "ThermalPhoton.h"
+#include "ParameterReader.h"
+
+class HadronGasPipiBremsstrahlung : public ThermalPhoton {
+ public:
+    HadronGasPipiBremsstrahlung(std::shared_ptr<ParameterReader> paraRdr_in);
+    ~HadronGasPipiBremsstrahlung() {}
+    void analyticRates(double T, double muB, std::vector<double> &Eq,
+                        std::vector<double> &eqrate_ptr);
+};
+
+#endif
