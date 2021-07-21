b480482567314f1dd8f438b2962af9339e659793
Abel Noble <abel.noble3@gmailc.om>
2021-07-21 14:41:15 -0400

    fixed mathematical calculations in Bremsstrahlung file

--- src/HadronGasPipiBremsstrahlung.cpp
+++ src/HadronGasPipiBremsstrahlung.cpp
@@ -12,7 +12,7 @@ void HadronGasPipiBremsstrahlung::analyticRates(double T, double muB, std::vecto
         double bbT = -35.54 + 414.8*T - 2054*T2 + 3718.8*T3;
         double ybT = 0.7364 - 10.72*T + 56.32*T2 - 103.5*T3;
         double dbT = -2.51 + 58.152*T - 318.24*T2 + 610.7*T3;
-        double logB = abT + bbT*Eq_local + ybT*Eq_local*Eq_local + dbT*(1/(Eq_local + 0.2));
+        double logB = abT + bbT*Eq_local + ybT*Eq_local*Eq_local + dbT/(Eq_local + 0.2);
         eqrate_ptr[i] = logB;
     }
 }
